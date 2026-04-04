e2dab1e37ef87617de4005e01a809f9805cc9839
Koventhan28 <k.koventhan@gmail.com>
2026-04-05 01:15:36 +0530

    created a new branch dev and pushed the modified files

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-IMAGE_NAME=$1
-TAG=$2
-
-echo "Building Docker image: $IMAGE_NAME:$TAG"
-docker build -t $IMAGE_NAME:$TAG .
+docker build -t koventhan/ecommercedev .
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Ensures the latest containers are running
 docker-compose down
-docker-compose pull
 docker-compose up -d
